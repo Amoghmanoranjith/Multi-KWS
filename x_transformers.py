dcd95840d54869fd3135145c83f1060d2ed5e356
Amoghmanoranjith Navade <130673768+Amoghmanoranjith@users.noreply.github.com>
2025-09-06 19:28:22 +0530

    Update x_transformers.py
    added quaternion feedforward

--- x_transformers.py
+++ x_transformers.py
@@ -1246,6 +1246,72 @@ class FeedForward(Module):
             out = out * deep_embed
 
         return out
+# qff
+class QuaternionFeedForward(Module):
+    def __init__(
+        self,
+        dim,
+        dim_out = None,
+        mult = 4,
+        glu = False,
+        glu_mult_bias = False,
+        swish = False,
+        relu_squared = False,
+        custom_activation = None,
+        post_act_ln = False,
+        dropout = 0.,
+        sublayer_dropout = 0.,
+        no_bias = False,
+        zero_init_output = False,
+    ):
+        super().__init__()
+        inner_dim = int(dim * mult)
+        dim_out = default(dim_out, dim)
+
+        if exists(custom_activation):
+            activation = deepcopy(custom_activation)
+        elif relu_squared:
+            activation = ReluSquared()
+        elif swish:
+            activation = nn.SiLU()
+        else:
+            activation = nn.GELU()
+
+        if glu:
+            proj_in = GLU(dim, inner_dim, activation, mult_bias = glu_mult_bias)
+        else:
+            proj_in = nn.Sequential(
+                QuaternionLinear(dim, inner_dim, bias = not no_bias),
+                activation
+            )
+
+        proj_out = QuaternionLinear(inner_dim, dim_out, bias = not no_bias)
+
+        self.ff = Sequential(
+            proj_in,
+            LayerNorm(inner_dim) if post_act_ln else None,
+            nn.Dropout(dropout),
+            proj_out,
+            nn.Dropout(sublayer_dropout) if sublayer_dropout > 0. else None
+        )
+
+        # init last linear layer to 0
+
+        if zero_init_output:
+            init_zero_(proj_out)
+
+    def forward(
+        self,
+        x,
+        deep_embed = None
+    ):
+        out = self.ff(x)
+
+        if exists(deep_embed):
+            out = out * deep_embed
+
+        return out
+
 
 # attention. it is all we need
 
