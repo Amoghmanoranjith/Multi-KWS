5068c463a3205c5fe1474faf9a486d91ba6fc916
Amoghmanoranjith Navade <130673768+Amoghmanoranjith@users.noreply.github.com>
2025-09-06 19:31:20 +0530

    Update x_transformers.py
    added quatLinear

--- x_transformers.py
+++ x_transformers.py
@@ -25,6 +25,7 @@ from attend import Attend, Intermediates
 import einx
 from einops.layers.torch import Rearrange
 from einops import rearrange, repeat, reduce, pack, unpack
+from quatOps import QuaternionLinearFunction
 
 # einstein notation
 
@@ -1246,6 +1247,66 @@ class FeedForward(Module):
             out = out * deep_embed
 
         return out
+
+# quaternion linear
+class QuaternionLinear(Module):
+    r"""Applies a quaternion linear transformation to the incoming data.
+    """
+
+    def __init__(self, in_features, out_features, bias=True,
+                 init_criterion='he', weight_init='quaternion',
+                 seed=None):
+
+        super(QuaternionLinear, self).__init__()
+        self.in_features  = in_features//4
+        self.out_features = out_features//4
+        self.r_weight     = Parameter(torch.Tensor(self.in_features, self.out_features))
+        self.i_weight     = Parameter(torch.Tensor(self.in_features, self.out_features))
+        self.j_weight     = Parameter(torch.Tensor(self.in_features, self.out_features))
+        self.k_weight     = Parameter(torch.Tensor(self.in_features, self.out_features))
+
+        if bias:
+            self.bias     = Parameter(torch.Tensor(self.out_features*4))
+        else:
+            self.register_parameter('bias', None)
+
+        self.init_criterion = init_criterion
+        self.weight_init    = weight_init
+        self.seed           = seed if seed is not None else np.random.randint(0,1234)
+        self.rng            = RandomState(self.seed)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        winit = {'quaternion': quaternion_init,
+                 'unitary': unitary_init}[self.weight_init]
+        if self.bias is not None:
+            self.bias.data.fill_(0)
+        affect_init(self.r_weight, self.i_weight, self.j_weight, self.k_weight, winit,
+                    self.rng, self.init_criterion)
+
+    def forward(self, input):
+        # See the autograd section for explanation of what happens here.
+        if input.dim() == 3:
+            T, N, C = input.size()
+            input  = input.view(T * N, C)
+            output = QuaternionLinearFunction.apply(input, self.r_weight, self.i_weight, self.j_weight, self.k_weight, self.bias)
+            output = output.view(T, N, output.size(1))
+        elif input.dim() == 2:
+            output = QuaternionLinearFunction.apply(input, self.r_weight, self.i_weight, self.j_weight, self.k_weight, self.bias)
+        else:
+            raise NotImplementedError
+
+        return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+            + 'in_features=' + str(self.in_features) \
+            + ', out_features=' + str(self.out_features) \
+            + ', bias=' + str(self.bias is not None) \
+            + ', init_criterion=' + str(self.init_criterion) \
+            + ', weight_init=' + str(self.weight_init) \
+            + ', seed=' + str(self.seed) + ')'
+
 # qff
 class QuaternionFeedForward(Module):
     def __init__(
